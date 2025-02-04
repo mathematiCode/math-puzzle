5302268c52a10c76090847b81d79e3cdf1654357
Julianna Messineo <160360849+mathematiCode@users.noreply.github.com>
2025-02-04 11:52:48 -0600

    moving typescript back to main

---
+++ src/components/Board.tsx
@@ -0,0 +1,65 @@
+import { ForwardedRef, forwardRef } from 'react';
+import BoardSection from './BoardSection.tsx';
+
+interface Dimensions {
+  width: number;
+  height: number;
+}
+
+interface BoardSectionData {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  valid: boolean;
+}
+
+interface BoardProps {
+  dimensions: Dimensions;
+  boardSections: BoardSectionData[];
+}
+
+const Board = forwardRef(
+  (props: BoardProps, ref: ForwardedRef<HTMLDivElement>) => {
+    const { dimensions, boardSections } = props;
+    const boardWidth = dimensions.width;
+
+    return (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateRows: '1fr',
+        }}
+      >
+        {boardSections.map((row: BoardSectionData[], index: number) => {
+          if (row.length === 1) {
+            return <BoardSection key={index} section={row[0]} />;
+          } else {
+            return (
+              <div
+                key={index}
+                style={{
+                  display: 'grid',
+                  gridTemplateRows: '1fr',
+                  gridTemplateColumns: `repeat(${boardWidth}, 1fr)`,
+                  width: 'min-content',
+                }}
+              >
+                {row.map((section: BoardSectionData) => {
+                  return (
+                    <BoardSection
+                      key={`${section.x},${section.y}`}
+                      section={section}
+                    />
+                  );
+                })}
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+);
+
+export default Board;
