ddda2b8f5fddca0e2d1ac462c886f0cbf3353f86
Julianna Messineo <160360849+mathematiCode@users.noreply.github.com>
2025-02-16 14:25:54 -0600

    converted PiecesInPlay context to TS

--- src/components/LandingSquare.tsx
+++ src/components/LandingSquare.tsx
@@ -1,6 +1,4 @@
 import { useDroppable } from '@dnd-kit/core';
-import { useContext } from 'react';
-import { CurrentLevelContext } from '../context/CurrentLevel.tsx';
 import { Unit } from './Unit.tsx';
 
 function LandingSquare({ id, color }: { id: string; color: string }) {
@@ -9,20 +7,7 @@ function LandingSquare({ id, color }: { id: string; color: string }) {
   });
 
   color = isOver ? 'white' : 'hsl(100, 65%, 92%)';
-  return (
-    <Unit key={id} color={color} ref={setNodeRef} id={id} />
-    // <div
-    //   className="unit"
-    //   key={id}
-    //   id={id}
-    //   style={{
-    //     backgroundColor: color,
-    //     width: `${sizeOfEachUnit}px`,
-    //     height: `${sizeOfEachUnit}px`,
-    //   }}
-    //
-    // />
-  );
+  return <Unit key={id} color={color} ref={setNodeRef} id={id} />;
 }
 
 export default LandingSquare;
