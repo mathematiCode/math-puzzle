eec520386d0e74fbe5bf3b5413d78d724745cbc7
Julianna Messineo <juliannamessineo@gmail.com>
2025-08-13 11:21:47 -0500

    refactor: commenting out currently used code

--- src/hooks/useClickAway.ts
+++ src/hooks/useClickAway.ts
@@ -1,15 +1,15 @@
 // Not currently in use YET
 
-import { useEffect } from 'react';
-function useClickAway(callback: () => void) {
-  useEffect(() => {
-    function handleClickAway(event: MouseEvent) {
-      console.log(event);
-    }
-    window.addEventListener('click', handleClickAway);
-    return () => {
-      window.removeEventListener('click', handleClickAway);
-    };
-  }, [callback]);
-}
-export default useClickAway;
+// import { useEffect } from 'react';
+// function useClickAway(callback: () => void) {
+//   useEffect(() => {
+//     function handleClickAway(event: MouseEvent) {
+//       console.log(event);
+//     }
+//     window.addEventListener('click', handleClickAway);
+//     return () => {
+//       window.removeEventListener('click', handleClickAway);
+//     };
+//   }, [callback]);
+// }
+// export default useClickAway;
--- src/hooks/useSnapToGrid.ts
+++ src/hooks/useSnapToGrid.ts
@@ -1,43 +1,44 @@
-// Not currently in use
+// Not currently in use but I may use it for ghosting to show where the piece will  be dropped on the board.
+
 // import { Modifier } from '@dnd-kit/core';
 // import { useMemo } from 'react';
 
-export function useSnapToGrid(
-  gridSize: number,
-  origin: { x: number; y: number }
-) {
-  // return useMemo(() => {
-  return ({ transform }: { transform: { x: number; y: number } }) => {
-    let xOffset = (transform.x - origin.x) % gridSize;
+// export function useSnapToGrid(
+//   gridSize: number,
+//   origin: { x: number; y: number }
+// ) {
+//   // return useMemo(() => {
+//   return ({ transform }: { transform: { x: number; y: number } }) => {
+//     let xOffset = (transform.x - origin.x) % gridSize;
 
-    if (xOffset > gridSize / 2) {
-      xOffset -= gridSize;
-    }
-    // if (xOffset < 0) {
-    //   xOffset += gridSize;
-    // }
+//     if (xOffset > gridSize / 2) {
+//       xOffset -= gridSize;
+//     }
+//     // if (xOffset < 0) {
+//     //   xOffset += gridSize;
+//     // }
 
-    let yOffset = (transform.y - origin.y) % gridSize;
+//     let yOffset = (transform.y - origin.y) % gridSize;
 
-    // if (yOffset < 0) {
-    //   yOffset += gridSize;
-    // }
+//     // if (yOffset < 0) {
+//     //   yOffset += gridSize;
+//     // }
 
-    console.log('origin', origin);
-    console.log('xOffset', xOffset);
-    console.log('yOffset', yOffset);
-    console.log('transform', transform);
-    console.log('gridSize', gridSize);
-    // let xOffset = 0;
-    // let yOffset = 0;
+//     console.log('origin', origin);
+//     console.log('xOffset', xOffset);
+//     console.log('yOffset', yOffset);
+//     console.log('transform', transform);
+//     console.log('gridSize', gridSize);
+//     // let xOffset = 0;
+//     // let yOffset = 0;
 
-    return {
-      ...transform,
-      x: Math.ceil(transform.x / gridSize) * gridSize + xOffset,
-      y: Math.ceil(transform.y / gridSize) * gridSize + yOffset,
-    };
-  };
-  // }, [gridSize, origin]);
-}
+//     return {
+//       ...transform,
+//       x: Math.ceil(transform.x / gridSize) * gridSize + xOffset,
+//       y: Math.ceil(transform.y / gridSize) * gridSize + yOffset,
+//     };
+//   };
+//   // }, [gridSize, origin]);
+// }
 
-export default useSnapToGrid;
+// export default useSnapToGrid;
